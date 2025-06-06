1b8dd82788c43a52f1a1e6b8069ac988dfe1186b
VenkatTejaC <tejuchalla017@gmail.com>
2025-06-06 10:17:03 +0100

    Delete schoolApp directory

--- schoolApp/admissions/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from admissions.views import addAdmission, admissionReport
-
-urlpatterns = [
-
-    path('newadm/',addAdmission),
-    path('admrpt/',admissionReport),
-
-]
